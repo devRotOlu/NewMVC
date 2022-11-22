d3ffceac017939034e0b0e8409ba7669b0585f26
Olumide Rotimi <rotimiolumide68@gmail.com>
2022-11-22 02:12:42 +0100

    Add controller to project

---
+++ lumiBookStore/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace lumiBookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Olumide";
+        }
+    }
+}
